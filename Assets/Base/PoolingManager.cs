9b9e7bd0ac5423aed3aa3f09db749f805b759411
HungOishiro <hungoishiro@gmail.com>
2023-02-04 10:17:23 +0700

    blank pj

---
+++ Assets/Base/PoolingManager.cs
@@ -0,0 +1,343 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public enum NamePrefabPool
+{
+    None
+}
+[System.Serializable]
+public enum PrefabPoolType
+{
+    VFX,
+    GAMEOBJECT
+}
+[System.Serializable]
+public class PoolingInfo
+{
+    public string name;
+    public PrefabPoolType prefabType;
+    public NamePrefabPool namePrefab;
+    public GameObject prefab;
+    public int countStartSpawn;
+}
+
+public class PoolingManager : SingletonMonoBehaviour<PoolingManager>
+{
+    [Header("VFX POOL OBJECTS")]
+    public PoolingInfo[] vfxPoolingInfo;
+    List<GameObject> listVFXPool = new List<GameObject>();
+
+    [Header("GAMEOBJECT POOL")]
+    public PoolingInfo[] gameObjectPoolingInfo;
+    List<GameObject> listObjectPool = new List<GameObject>();
+
+    private void Start()
+    {
+        StartSpawnObject();
+    }
+
+    private void OnValidate()
+    {
+        if(vfxPoolingInfo.Length > 0)
+        {
+            foreach (PoolingInfo pl in vfxPoolingInfo)
+            {
+                pl.name = pl.namePrefab.ToString();
+            }
+        }
+
+        if(gameObjectPoolingInfo.Length > 0)
+        {
+            foreach (PoolingInfo pl in gameObjectPoolingInfo)
+            {
+                pl.name = pl.namePrefab.ToString();
+            }
+        }
+    }
+
+    public void StartSpawnObject()
+    {
+        foreach (PoolingInfo pooling in vfxPoolingInfo)
+        {
+            if (pooling.prefabType == PrefabPoolType.VFX)
+            {
+                for (int i = 0; i < pooling.countStartSpawn; i++)
+                {
+                    GameObject obj = Instantiate(pooling.prefab, transform);
+                    obj.name = pooling.namePrefab.ToString();
+                    obj.SetActive(false);
+                    listVFXPool.Add(obj);
+                }
+            }
+        }
+
+        foreach (PoolingInfo pooling in gameObjectPoolingInfo)
+        {
+            if (pooling.prefabType == PrefabPoolType.GAMEOBJECT)
+            {
+                for (int i = 0; i < pooling.countStartSpawn; i++)
+                {
+                    GameObject obj = Instantiate(pooling.prefab, transform);
+                    obj.name = pooling.namePrefab.ToString();
+                    obj.SetActive(false);
+                    listObjectPool.Add(obj);
+                }
+            }
+        }
+    }
+
+    public GameObject GetVFX(NamePrefabPool name)
+    {
+        if (listVFXPool.Count > 0)
+        {
+            foreach (GameObject obj in listVFXPool)
+            {
+                if (!obj.activeSelf && obj.name == name.ToString())
+                {
+                    obj.SetActive(true);
+                    return obj;
+                }
+            }
+
+            for (int i = 0; i < vfxPoolingInfo.Length; i++)
+            {
+                if (name == vfxPoolingInfo[i].namePrefab)
+                {
+                    GameObject vfx = Instantiate(vfxPoolingInfo[i].prefab, transform);
+                    vfx.name = name.ToString();
+                    listVFXPool.Add(vfx);
+                    return vfx;
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < vfxPoolingInfo.Length; i++)
+            {
+                if (name == vfxPoolingInfo[i].namePrefab)
+                {
+                    GameObject vfx = Instantiate(vfxPoolingInfo[i].prefab, transform);
+                    vfx.name = name.ToString();
+                    listVFXPool.Add(vfx);
+                    return vfx;
+                }
+            }
+        }
+
+        return null;
+    }
+    public GameObject GetVFX(NamePrefabPool name, Vector3 position, Transform parent = null)
+    {
+        if (listVFXPool.Count > 0)
+        {
+            foreach (GameObject obj in listVFXPool)
+            {
+                if (!obj.activeSelf && obj.name == name.ToString())
+                {
+                    if (parent == null)
+                    {
+                        obj.transform.SetParent(transform);
+                    }
+                    else
+                    {
+                        obj.transform.SetParent(parent);
+                    }
+                    obj.transform.position = position;
+                    obj.SetActive(true);
+                    return obj;
+                }
+            }
+
+            for (int i = 0; i < vfxPoolingInfo.Length; i++)
+            {
+                if (name == vfxPoolingInfo[i].namePrefab)
+                {
+                    GameObject vfx = Instantiate(vfxPoolingInfo[i].prefab, transform);
+                    vfx.name = name.ToString();
+                    if (parent == null)
+                    {
+                        vfx.transform.SetParent(transform);
+                    }
+                    else
+                    {
+                        vfx.transform.SetParent(parent);
+                    }
+                    vfx.transform.position = position;
+                    listVFXPool.Add(vfx);
+                    return vfx;
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < vfxPoolingInfo.Length; i++)
+            {
+                if (name == vfxPoolingInfo[i].namePrefab)
+                {
+                    GameObject vfx = Instantiate(vfxPoolingInfo[i].prefab, transform);
+                    vfx.name = name.ToString();
+                    if (parent == null)
+                    {
+                        vfx.transform.SetParent(transform);
+                    }
+                    else
+                    {
+                        vfx.transform.SetParent(parent);
+                    }
+
+                    vfx.transform.position = position;
+                    listVFXPool.Add(vfx);
+                    return vfx;
+                }
+            }
+        }
+
+        return null;
+    }
+    public GameObject GetObject(NamePrefabPool name, Vector3 position, Transform parent = null)
+    {
+        if (listObjectPool.Count > 0)
+        {
+            foreach (GameObject obj in listObjectPool)
+            {
+                if (!obj.activeSelf && obj.name == name.ToString())
+                {
+                    if (parent == null)
+                    {
+                        obj.transform.SetParent(transform);
+                    }
+                    else
+                    {
+                        obj.transform.SetParent(parent);
+                    }
+                    obj.transform.position = position;
+                    obj.SetActive(true);
+                    return obj;
+                }
+            }
+
+            for (int i = 0; i < gameObjectPoolingInfo.Length; i++)
+            {
+                if (name == gameObjectPoolingInfo[i].namePrefab)
+                {
+                    GameObject vfx = Instantiate(gameObjectPoolingInfo[i].prefab, transform);
+                    vfx.name = name.ToString();
+                    if (parent == null)
+                    {
+                        vfx.transform.SetParent(transform);
+                    }
+                    else
+                    {
+                        vfx.transform.SetParent(parent);
+                    }
+
+                    vfx.transform.position = position;
+                    listObjectPool.Add(vfx);
+                    return vfx;
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < gameObjectPoolingInfo.Length; i++)
+            {
+                if (name == gameObjectPoolingInfo[i].namePrefab)
+                {
+                    GameObject vfx = Instantiate(gameObjectPoolingInfo[i].prefab, transform);
+                    vfx.name = name.ToString();
+                    if (parent == null)
+                    {
+                        vfx.transform.SetParent(transform);
+                    }
+                    else
+                    {
+                        vfx.transform.SetParent(parent);
+                    }
+                    vfx.transform.position = position;
+                    listObjectPool.Add(vfx);
+                    return vfx;
+                }
+            }
+        }
+
+        return null;
+    }
+    public GameObject GetObject(NamePrefabPool name)
+    {
+        if (listObjectPool.Count > 0)
+        {
+            foreach (GameObject obj in listObjectPool)
+            {
+                if (!obj.activeSelf && obj.name == name.ToString())
+                {
+                    obj.SetActive(true);
+                    return obj;
+                }
+            }
+
+            for (int i = 0; i < gameObjectPoolingInfo.Length; i++)
+            {
+                if (name == gameObjectPoolingInfo[i].namePrefab)
+                {
+                    GameObject vfx = Instantiate(gameObjectPoolingInfo[i].prefab, transform);
+                    vfx.name = name.ToString();
+                    listObjectPool.Add(vfx);
+                    return vfx;
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < gameObjectPoolingInfo.Length; i++)
+            {
+                if (name == gameObjectPoolingInfo[i].namePrefab)
+                {
+                    GameObject vfx = Instantiate(gameObjectPoolingInfo[i].prefab, transform);
+                    vfx.name = name.ToString();
+                    listObjectPool.Add(vfx);
+                    return vfx;
+                }
+            }
+        }
+
+        return null;
+    }
+    public void DisableAllObject()
+    {
+        StopAllCoroutines();
+
+        for (int i = listObjectPool.Count - 1; i >= 0; i--)
+        {
+            if (listObjectPool[i] == null)
+            {
+                listObjectPool.RemoveAt(i);
+            }
+        }
+
+        for (int i = listVFXPool.Count - 1; i >= 0; i--)
+        {
+            if (listVFXPool[i] == null)
+            {
+                listVFXPool.RemoveAt(i);
+            }
+        }
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
+    }
+
+    public void DisableObjectWithTime(GameObject obj, float time)
+    {
+        StartCoroutine(TimeDisableObject(obj, time));
+    }
+
+    IEnumerator TimeDisableObject(GameObject obj, float time)
+    {
+        yield return new WaitForSeconds(time);
+        obj.SetActive(false);
+    }
+}
---
+++ Assets/Base/SceneLoadManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class SceneLoadManager : SingletonMonoBehaviour<SceneLoadManager>
+{
+    public bool isLoading;
+    public bool isUnloading;
+
+    private readonly string nameSceneGamePlay = "GamePlay";
+    private readonly string nameSceneHome = "Home";
+
+    public void LoadSceneWithName(string name)
+    {
+        LoadAsyncScene(name);
+    }
+
+    // public void LoadCurrentScene()
+    // {
+    //     string name = SceneManager.GetActiveScene().name;
+
+    //     LoadSceneWithName(name);
+    // }
+
+    public void LoadSceneHome()
+    {
+        LoadAsyncScene(nameSceneHome);
+    }
+
+    public void LoadSceneGamePlay()
+    {
+        LoadAsyncScene(nameSceneGamePlay);
+    }
+
+    // public void LoadSceneLevel(int index)
+    // {
+    //     if (index > GameConfig.TotalLevel)
+    //     {
+    //         index = GameConfig.TotalLevel;
+    //     }
+
+    //     LoadSceneWithName(nameSceneGamePlay + index);
+    // }
+
+    #region LoadSceneAsync
+    private void LoadAsyncScene(string name)
+    {
+        isLoading = true;
+        StartCoroutine(WaitLoadAsyncScene(name));
+    }
+
+    private IEnumerator WaitLoadAsyncScene(string name)
+    {
+        Debug.Log("Scene " + name + " is loading...");
+
+        yield return new WaitForSecondsRealtime(0.5f);
+
+        var asyncLoad = SceneManager.LoadSceneAsync(name);
+
+        while (asyncLoad == null || !asyncLoad.isDone) yield return null;
+
+        if (isLoading)
+        {
+            isLoading = false;
+
+            Debug.Log("Scene " + name + " is loaded!!!");
+        }
+    }
+
+
+    public void UnloadSceneWithName(string name, Action _callBack = null)
+    {
+        UnloadAsyncScene(name, _callBack);
+    }
+    private void UnloadAsyncScene(string name, Action _callBack = null)
+    {
+        isUnloading = true;
+        StartCoroutine(WaitUnloadScene(name, _callBack));
+    }
+
+    private IEnumerator WaitUnloadScene(string name, Action _callBack = null)
+    {
+        var asyncLoad = SceneManager.UnloadSceneAsync(name);
+
+        while (!asyncLoad.isDone) yield return null;
+
+        if (isUnloading)
+        {
+            isUnloading = false;
+
+            _callBack?.Invoke();
+
+            Debug.Log("Scene " + name + " is unloaded!!!");
+        }
+    }
+
+    #endregion
+}
